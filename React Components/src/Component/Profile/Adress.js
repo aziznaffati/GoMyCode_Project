e87fe40aec0221172a9574b84905ea2eac3206dd
aziznaffati <aziznaffati9@gmail.com>
2021-11-16 21:18:12 +0100

    make it better

---
+++ React Components/src/Component/Profile/Adress.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const Adress=()=>{
+    let palce='Ben Arous';
+    return (
+        <div>I am from {palce},Tunisia</div>
+    )
+}
+export default Adress;
---
+++ React Components/src/Component/Profile/FullName.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FullName=()=>{
+    let firstName='Mohamed Aziz';
+    let lastName='Naffati';
+    return (
+        <div>
+            Welcome 
+            my name is {firstName +' '+lastName}
+        </div>
+    )
+}
+export default FullName;
